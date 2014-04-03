d343e4562819e5d6eb9ca64ff24bddc33c80df47
ojg <ojg@users.noreply.github.com>
2014-04-03 23:00:43 +0200

    refactor switch into array lookup

--- dsp.c
+++ dsp.c
@@ -5,6 +5,31 @@
 #include <stdio.h>
 #include "dsp.h"
 
+enum {
+    GAIN_OPT = 0,
+    GATE_OPT,
+    IIR_OPT,
+    CLIP_OPT,
+    END_OPT
+};
+
+char *const token[] = {
+    [GAIN_OPT]   = "gain",
+    [GATE_OPT]   = "gate",
+    [IIR_OPT]    = "iir",
+    [CLIP_OPT]   = "clip",
+    NULL
+};
+
+int (*createfunc[])(struct qdsp_t * dsp, char ** subopts) = {
+        [GAIN_OPT]   = create_gain,
+        [GATE_OPT]   = create_gate,
+        [IIR_OPT]    = create_iir,
+        [CLIP_OPT]   = create_clip,
+        NULL
+};
+
+
 void endprogram(char * str)
 {
     fprintf(stderr,"%s",str);
@@ -13,41 +38,20 @@ void endprogram(char * str)
 
 void create_dsp(struct qdsp_t * dsp, char * subopts)
 {
-    enum {
-        GAIN_OPT = 0,
-        GATE_OPT,
-        IIR_OPT,
-        CLIP_OPT,
-    };
-    char *const token[] = {
-        [GAIN_OPT]   = "gain",
-        [GATE_OPT]   = "gate",
-        [IIR_OPT]    = "iir",
-        [CLIP_OPT]   = "clip",
-        NULL
-    };
     char *value;
     char *name = NULL;
     int errfnd = 0;
+    int curtoken;
+
     fprintf(stderr,"create_dsp subopts: %s\n", subopts);
 
     while (*subopts != '\0' && !errfnd) {
-        switch (getsubopt(&subopts, token, &value)) {
-        case GATE_OPT:
-            errfnd = create_gate(dsp, &subopts);
-            break;
-        case GAIN_OPT:
-            errfnd = create_gain(dsp, &subopts);
-            break;
-        case IIR_OPT:
-            errfnd = create_iir(dsp, &subopts);
-            break;
-        case CLIP_OPT:
-            errfnd = create_clip(dsp, &subopts);
-            break;
-        default:
-            fprintf(stderr, "create_dsp: No match found "
-                    "for token: /%s/\n", value);
+        curtoken = getsubopt(&subopts, token, &value);
+        if (curtoken >= 0 && curtoken < END_OPT) {
+            errfnd = createfunc[curtoken](dsp, &subopts);
+        }
+        else {
+            fprintf(stderr, "create_dsp: No match found for token: /%s/\n", value);
             errfnd = 1;
             break;
         }
