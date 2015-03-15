32935e275ee9e9b829c6f5a7abfb1fc878bf7154
ojg <ojg@users.noreply.github.com>
2015-03-16 00:22:41 +0100

    Add feexcept to file-qdsp

--- file-qdsp.c
+++ file-qdsp.c
@@ -298,7 +298,7 @@ int main (int argc, char *argv[])
         dsp = process(nframes, dsphead);
         clock_gettime(CLOCK_THREAD_CPUTIME_ID, &t2); t = timespecsub(t,t2); ttot = timespecadd(t,ttot);
         raised = fetestexcept(FE_INEXACT | FE_DIVBYZERO | FE_UNDERFLOW | FE_OVERFLOW | FE_INVALID);
-        if (raised) debugprint(3, "FE exception rasied: %d\n", raised);
+        if (raised) debugprint(3, "FE exception raised: 0x%02X\n", raised);
         debugprint(3, "outbufs=%p\n", dsp->outbufs[0]);
         interleave(writebuf, dsp->outbufs[0], channels, nframes);
         if (nframesread != sf_writef_float(output_file, writebuf, nframesread))
