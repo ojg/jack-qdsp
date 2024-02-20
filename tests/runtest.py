f534d8d516866a32203a5fe2b288f82b2c438681
ojg <ojg@github.com>
2024-02-20 18:56:31 +0100

    Rewrite dsp-gate and add tests

--- tests/runtest.py
+++ tests/runtest.py
@@ -12,11 +12,11 @@ def readaudio():
     return alab.wavread("test_out.wav")[0]
 
 
-def compareaudio(data1, data2):
-    if (abs(data1 - data2) > 1e-7).any():
+def compareaudio(data1, data2, threshold=1e-7):
+    if (abs(data1 - data2) > threshold).any():
         print "Fail"
         for i in range(size(data1)):
-            print array((data1[i], data2[i]))
+            print array((i, data1[i], data2[i]))
         quit()
     else:
         print "Pass"
@@ -51,8 +51,45 @@ def test_gain():
     compareaudio(expected, readaudio())
 
 
+def test_gate():
+    print "Testing dsp-gate"
+
+    ref = linspace(-0.25, 0.25, 200)
+    writeaudio(ref)
+
+    # test open gate
+    expected = ref # * array((zeros(50), ones(100), zeros(50))
+    os.system("../file-qdsp -n 64 -i test_in.wav -o test_out.wav -p gate,t=-120")
+    compareaudio(expected, readaudio())
+
+    #test closing gate with zero hold
+    expected = ref * concatenate((ones(64), linspace(1,0,64), zeros(72)))
+    os.system("../file-qdsp -n 64 -i test_in.wav -o test_out.wav -p gate,t=-6")
+    compareaudio(expected, readaudio(), 2e-7)
+
+    #test closing gate with hold
+    expected = ref * concatenate((ones(64), ones(64), linspace(1,0,64), zeros(8)))
+    os.system("../file-qdsp -n 64 -i test_in.wav -o test_out.wav -p gate,t=-6,h=0.003")
+    compareaudio(expected, readaudio(), 2e-7)
+
+    #test closing and opening gate with zero hold
+    ref = linspace(-0.25, 1, 256)
+    writeaudio(ref)
+    expected = ref * concatenate((ones(64), linspace(1,0,64), zeros(64), linspace(0,1,64)))
+    os.system("../file-qdsp -n 64 -i test_in.wav -o test_out.wav -p gate,t=-6")
+    compareaudio(expected, readaudio(), 2e-7)
+
+    #test closing and opening gate with hold
+    ref = linspace(-0.25, 1, 384)
+    writeaudio(ref)
+    expected = ref * concatenate((ones(64), ones(64), linspace(1,0,64), zeros(64), linspace(0,1,64), ones(64)))
+    os.system("../file-qdsp -n 64 -i test_in.wav -o test_out.wav -p gate,t=-6,h=0.003")
+    compareaudio(expected, readaudio(), 2e-7)
+
+
 def main():
-    test_gain()
+    ret = test_gain()
+    ret = test_gate()
     os.remove('test_in.wav')
     os.remove('test_out.wav')
 
